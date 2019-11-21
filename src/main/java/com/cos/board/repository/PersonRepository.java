9c11678f272ed727074e6c1dcc85176c7bafddd4
DAVIS PARK <zinuriu2@gmail.com>
2019-11-21 17:16:14 +0900

    SpringBoot-MyBatis-CRUD

---
+++ src/main/java/com/cos/board/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.cos.board.repository;
+
+import java.util.List;
+
+import com.cos.board.model.Person;
+
+public interface PersonRepository {
+	Person findById(int id);
+	List<Person> findAll();
+	void save(Person person);
+	void delete(int id);
+	void update(Person person);
+}
